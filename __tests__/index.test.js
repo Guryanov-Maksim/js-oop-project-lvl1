d5ec01c4fe2964ddf40bf5eccece52870214ffca
Maksim-Guryanov <guryanov-mv@yandex.ru>
2022-07-21 12:43:35 +0300

    feat: update tests (number validator)

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -59,3 +59,74 @@ test('string, several checks', () => {
   expect(schema.isValid('does the fox say')).toBe(false);
   expect(schema.isValid('that does the fox say')).toBe(false);
 });
+
+test('number, required', () => {
+  const v = new Validator();
+  const schema = v.number();
+
+  expect(schema.isValid('')).toBe(false);
+  expect(schema.isValid(null)).toBe(true);
+  expect(schema.isValid()).toBe(false);
+
+  schema.required();
+  expect(schema.isValid(null)).toBe(false);
+  expect(schema.isValid(0)).toBe(true);
+  expect(schema.isValid(5)).toBe(true);
+  expect(schema.isValid(-5)).toBe(true);
+});
+
+test('number, positive', () => {
+  const v = new Validator();
+  const schema = v.number();
+
+  schema.positive();
+  expect(schema.isValid(null)).toBe(false);
+  expect(schema.isValid(-1)).toBe(false);
+  expect(schema.isValid(0)).toBe(true);
+  expect(schema.isValid(1)).toBe(true);
+});
+
+test('number, range', () => {
+  const v = new Validator();
+  const schema1 = v.number();
+
+  schema1.range(0, 10);
+  expect(schema1.isValid(null)).toBe(false);
+  expect(schema1.isValid(-1)).toBe(false);
+  expect(schema1.isValid(0)).toBe(true);
+  expect(schema1.isValid(5)).toBe(true);
+  expect(schema1.isValid(10)).toBe(true);
+  expect(schema1.isValid(11)).toBe(false);
+
+  const schema2 = v.number();
+  schema2.range(-5, 5);
+  expect(schema2.isValid(-6)).toBe(false);
+  expect(schema2.isValid(-5)).toBe(true);
+  expect(schema2.isValid(-4)).toBe(true);
+  expect(schema2.isValid(0)).toBe(true);
+  expect(schema2.isValid(4)).toBe(true);
+  expect(schema2.isValid(5)).toBe(true);
+  expect(schema2.isValid(6)).toBe(false);
+
+  const schema3 = v.number();
+  schema3.range(-10, -5);
+  expect(schema3.isValid(-11)).toBe(false);
+  expect(schema3.isValid(-10)).toBe(true);
+  expect(schema3.isValid(-5)).toBe(true);
+  expect(schema3.isValid(-4)).toBe(false);
+  expect(schema3.isValid(0)).toBe(false);
+});
+
+test('number, several checks', () => {
+  const v = new Validator();
+  const schema = v.number();
+
+  schema.required().positive().range(1, 10);
+  expect(schema.isValid(null)).toBe(false);
+  expect(schema.isValid(-1)).toBe(false);
+  expect(schema.isValid(0)).toBe(false);
+  expect(schema.isValid(1)).toBe(true);
+  expect(schema.isValid(5)).toBe(true);
+  expect(schema.isValid(10)).toBe(true);
+  expect(schema.isValid(11)).toBe(false);
+});
