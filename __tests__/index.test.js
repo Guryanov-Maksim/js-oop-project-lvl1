10333c3b7bfed9b408fb2d0f220b19e0e1ad4658
Maksim-Guryanov <guryanov-mv@yandex.ru>
2022-07-21 14:50:01 +0300

    feat: update tests (object validator)

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -80,7 +80,7 @@ test('number, positive', () => {
   const schema = v.number();
 
   schema.positive();
-  expect(schema.isValid(null)).toBe(false);
+  expect(schema.isValid(null)).toBe(true);
   expect(schema.isValid(-1)).toBe(false);
   expect(schema.isValid(0)).toBe(true);
   expect(schema.isValid(1)).toBe(true);
@@ -168,4 +168,21 @@ test('array, several checks', () => {
   expect(schema.isValid([1, 2])).toBe(false);
   expect(schema.isValid([1, 2, 3])).toBe(true);
   expect(schema.isValid([1, 2, 3, 4])).toBe(false);
+});
+
+test('object, shape', () => {
+  const v = new Validator();
+  const schema = v.object();
+
+  schema.shape({
+    name: v.string().required(),
+    age: v.number().positive(),
+  });
+  expect(schema.isValid({ name: 'kolya', age: 100 })).toBe(true);
+  expect(schema.isValid({ name: 'maya', age: null })).toBe(true);
+  expect(schema.isValid({ name: '', age: null })).toBe(false);
+  expect(schema.isValid({ name: 'ada', age: -5 })).toBe(false);
+  expect(schema.isValid({ name: 'ada', age: -5, surname: 'petrov' })).toBe(false);
+  expect(schema.isValid({ name: 'ada', surname: 'petrov' })).toBe(false);
+  expect(schema.isValid()).toBe(false);
 });
